8a59b2524e4044090d8ed6b959ea3269b79ee12a
Paulo Rogerio Eloy <pauloreloy@gmail.com>
2025-05-26 21:49:01 -0300

    new image

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 338966484167.dkr.ecr.us-east-1.amazonaws.com
 docker build -t paulodatarib/orquestrador .
 docker tag paulodatarib/orquestrador:latest 338966484167.dkr.ecr.us-east-1.amazonaws.com/paulodatarib/orquestrador:latest
 docker push 338966484167.dkr.ecr.us-east-1.amazonaws.com/paulodatarib/orquestrador:latest
